af4f74052cf673f5486078e41a6140135db5ebbb
Jayanta Mondal <jayanta551905@gmail.com>
2023-09-21 00:55:39 +0600

    auth update

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -4,7 +4,7 @@ import { useSession, signOut } from "next-auth/react";
 const TopNav = () => {
   const { data, status } = useSession();
 
-  console.log("========>", "data => ", data, " status => ", status);
+  // console.log("========>", "data => ", data, " status => ", status);
 
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
